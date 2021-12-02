9292f73abbfb1145b02b83f0a6d895770e1f18fa
jagadhis <jwaran78@gmail.com>
2021-12-02 16:17:09 +0530

    commit

---
+++ src/Components/Remaining.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Remaining = () => {
+  return (
+    <div className="alert alert-success">
+      <span>Remaining : Rs.2000</span>
+    </div>
+  );
+};
+
+export default Remaining;

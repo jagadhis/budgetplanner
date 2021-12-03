78b30d82f0cdb0ed0bb781db4e8db4787e3c079a
jagadhis <jwaran78@gmail.com>
2021-12-03 10:28:58 +0530

    alignment done

--- src/Components/AddExpense.js
+++ src/Components/AddExpense.js
@@ -21,7 +21,7 @@ const AddExpense = () => {
   return (
     <form onSubmit={onSubmit}>
       <div className="row">
-        <div className="col-sm">
+        <div className="col-sm col-lg-4">
           <label for="name">Name</label>
           <input
             required="required"
@@ -32,7 +32,7 @@ const AddExpense = () => {
             onChange={(event) => setName(event.target.value)}
           ></input>
         </div>
-        <div className="col-sm">
+        <div className="col-sm col-lg-4">
           <label for="cost">Cost</label>
           <input
             required="required"
@@ -43,6 +43,8 @@ const AddExpense = () => {
             onChange={(event) => setCost(event.target.value)}
           ></input>
         </div>
+      </div>
+      <div className="row mt-3">
         <div className="col-sm">
           <button type="submit" className="btn btn-primary">
             Add
